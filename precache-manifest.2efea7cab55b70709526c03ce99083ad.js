ccceb38499b36041615dd6b716850340b2e13e31
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-09-23 02:14:04 +0530

    Updates

---
+++ precache-manifest.2efea7cab55b70709526c03ce99083ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6b5cf87e93dd5af0f59f667c202b67",
+    "url": "/youtube-browser-reactjs/index.html"
+  },
+  {
+    "revision": "f0f828efe5ee05897555",
+    "url": "/youtube-browser-reactjs/static/js/2.0c2fcab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-browser-reactjs/static/js/2.0c2fcab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23503597f0830fbc805b",
+    "url": "/youtube-browser-reactjs/static/js/main.29719e90.chunk.js"
+  },
+  {
+    "revision": "c9b28dff42fab4fbde64",
+    "url": "/youtube-browser-reactjs/static/js/runtime-main.d3b075de.js"
+  }
+]);
